6fe33b230ea7bc050b854d97c9bdadf8e30e1d0f
albertbahia <albert.bahia1@gmail.com>
2016-02-05 12:16:13 -0500

    added README, basic HTML setup and pseudo code for JS

---
+++ week-4-jquery-rpg/rpgStarWars.js
@@ -0,0 +1,9 @@
+// Pseudo code it out
+
+// 1. Create 4 characters with objects.
+
+// 2. Create functions to enable actions between objects.
+
+// 3. Render character info to the DOM.
+
+// 4. Render results of the actions between the characters to the DOM.
