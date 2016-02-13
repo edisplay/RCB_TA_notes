d7b64b1c0e50f34725fde8f28ca7a2880a4d1a54
Owens O'Brien <owensobrien@gmail.com>
2016-02-12 19:13:14 -0500

    basic timeout flow

--- week-5-timers-trivia/homework_solution/js/app.js
+++ week-5-timers-trivia/homework_solution/js/app.js
@@ -4,7 +4,13 @@ window.onload = function(){
 
 };
 
-
+$(document).on('click', $('#start-over') , function() {
+  game.currentQuestion = 0;
+  game.counter = 3; ////////////////////////CHANGE ME
+  game.correct = 0;
+  game.incorrect = 0;
+  game.loadQuestion();
+});
 
 var questions = [{
   question: "What was the first full length CGI movie?",
@@ -55,34 +61,56 @@ var panel = $('#quiz-area');
 var game = {
   questions:questions,
   currentQuestion:0,
-  counter:3,
+  counter:3, ////////////////////////CHANGE ME
   correct:0,
   incorrect:0,
   timeCount: function(){
     timer = setInterval(game.countdown, 1000);
   },
-  setupGame: function(){
-    this.timeCount();
-    panel.append('<h2>' + questions[0].question + '</h2>' );
-    for (var i = 0; i<questions[0].answers.length; i++){
-      panel.append('<button>' + questions[0].answers[i]+ '</button>');
-    }
-  },
   countdown: function(){
+    //Not sure if we're teaching them how to save the state of "this" to be used elsewhere.
     console.log(game.counter);
     game.counter--;
     $('#counter-number').html(game.counter);
 
     if (game.counter === 0){
-      game.nextQuestion();
+      console.log('TIME UP');
+      game.timeUp();
     }
   },
-  nextQuestion: function (){
+  loadQuestion: function(){
+    this.timeCount();
+    panel.html('<h2>' + questions[this.currentQuestion].question + '</h2>' );
+    for (var i = 0; i<questions[this.currentQuestion].answers.length; i++){
+      panel.append('<button>' + questions[this.currentQuestion].answers[i]+ '</button>');
+    }
+  },
+  nextQuestion: function(){
+    //I'm a little worried they might be confused as to why they cant use "this" here
+    console.log('question reset');
+    game.counter = 3; ////////////////////////CHANGE ME
+    game.currentQuestion++;
+    game.loadQuestion();
+  },
+  timeUp: function (){
     clearInterval(timer);
     panel.html('<h2>Out of Time!</h2>');
     panel.append('<h3>The Correct Answer was:' + questions[this.currentQuestion].correctAnswer);
     panel.append('<img src="' + questions[this.currentQuestion].image + '" />');
+
+    if (game.currentQuestion === questions.length - 1){
+      setTimeout(game.results, 3 * 1000);////////////////////////CHANGE ME
+    } else {
+      //figured it would be good to also include setTimeout
+      setTimeout(game.nextQuestion, 3 * 1000);////////////////////////CHANGE ME
+    }
+  },
+  results: function() {
+    panel.html('<h2>All done, heres how you did!</h2>');
+    panel.append('<h3>Correct Answers: ' + game.correct + '</h3>');
+    panel.append('<h3>incorrect Answers: ' + game.incorrect + '</h3>');
+    panel.append('<h2 id="start-over">Start Over?</h2>');
   }
 };
 
-game.setupGame();
+game.loadQuestion();
