1539962302a55ac200f2e02e09cd97d50beba898
Owens O'Brien <owensobrien@gmail.com>
2016-02-12 21:07:40 -0500

    fixed interval bug

--- week-5-timers-trivia/homework_solution/js/app.js
+++ week-5-timers-trivia/homework_solution/js/app.js
@@ -4,6 +4,8 @@ window.onload = function(){
 
 };
 
+var panel = $('#quiz-area');
+var countStartNumber = 20;
 
 
 ///////////////////////////////////////////////////////////////////////////////
@@ -12,21 +14,16 @@ window.onload = function(){
 
 ///////////////////////////////////////////////////////////////////////////////
 
-$(document).on('click', $('#start-over') , function() {
+$('#start-over').on('click', function() {
   game.currentQuestion = 0;
-  game.counter = 3; ////////////////////////CHANGE ME
+  game.counter = countStartNumber;
   game.correct = 0;
   game.incorrect = 0;
   game.loadQuestion();
 });
 
 $(document).on('click', $('.answer-button'), function(e) {
-  clearInterval(timer);
-  if ($(e.target).data("id") == questions[game.currentQuestion].correctAnswer){
-    game.answeredCorrectly();
-  } else {
-    game.answeredIncorrectly();
-  }
+  game.clicked(e);
 });
 
 
@@ -41,61 +38,57 @@ $(document).on('click', $('.answer-button'), function(e) {
 var questions = [{
   question: "What was the first full length CGI movie?",
   answers: ["A Bug's Life", "Monsters Inc.", "Toy Story", "The Lion King"],
-  correctAnswer: 3,
+  correctAnswer: "Toy Story",
   image:"img/toystory.gif"
 }, {
   question: "Which of these is NOT a name of one of the Spice Girls?",
   answers: ["Sporty Spice", "Fred Spice", "Scary Spice", "Posh Spice"],
-  correctAnswer: 2,
+  correctAnswer: "Fred Spice",
   image:"img/spicegirls.gif"
 }, {
   question: "Which NBA team won the most titles in the 90s?",
   answers: ["New York Knicks", "Portland Trailblazers", "Los Angeles Lakers", "Chicago Bulls"],
-  correctAnswer: 4,
+  correctAnswer: "Chicago Bulls",
   image:"img/bulls.gif"
 }, {
   question: 'Which group released the hit song, "Smells Like Teen Spirit"?',
   answers: ["Nirvana", "Backstreet Boys", "The Offspring", "No Doubt"],
-  correctAnswer: 1,
+  correctAnswer: "Nirvana",
   image:"img/nirvanabark.gif"
 }, {
   question: 'Which popular Disney movie featured the song, "Circle of Life"?',
   answers: ["Aladdin", "Hercules", "Mulan", "The Lion King"],
-  correctAnswer: 4,
+  correctAnswer: "The Lion King",
   image:"img/lionking.gif"
 }, {
   question: 'Finish this line from the Fresh Prince of Bel-Air theme song: "I whistled for a cab and when it came near, the liscense plate said..."',
   answers: ["Dice", "Mirror", "Fresh", "Cab"],
-  correctAnswer: 3,
+  correctAnswer: "Fresh",
   image:"img/fresh.gif"
 }, {
   question: "What was Doug's best friend's name?",
   answers: ["Skeeter", "Mark", "Zach", "Cody"],
-  correctAnswer: 1,
+  correctAnswer: "Skeeter",
   image:"img/skeeter.gif"
 }, {
   question: "What was the name of the principal at Bayside High in Saved By The Bell?",
   answers: ["Mr.Zhou", "Mr.Driggers", "Mr.Belding", "Mr.Page"],
-  correctAnswer: 3,
+  correctAnswer: "Mr.Belding",
   image:"img/belding.gif"
 }];
 
 
-var panel = $('#quiz-area');
 
 
 var game = {
   questions:questions,
   currentQuestion:0,
-  counter:3, ////////////////////////CHANGE ME
+  counter:countStartNumber,
   correct:0,
   incorrect:0,
-  timeCount: function(){
-    timer = setInterval(game.countdown, 1000);
-  },
   countdown: function(){
     //Not sure if we're teaching them how to save the state of "this" to be used elsewhere.
-    console.log(game.counter);
+    console.log('set interval running');
     game.counter--;
     $('#counter-number').html(game.counter);
 
@@ -105,21 +98,23 @@ var game = {
     }
   },
   loadQuestion: function(){
-    this.timeCount();
+    timer = setInterval(game.countdown, 1000);
     panel.html('<h2>' + questions[this.currentQuestion].question + '</h2>' );
     for (var i = 0; i<questions[this.currentQuestion].answers.length; i++){
-      panel.append('<button class="answer-button" id="button"' + 'data-id="' + i + '">' + questions[this.currentQuestion].answers[i]+ '</button>');
+      panel.append('<button class="answer-button" id="button"' + 'data-name="' + questions[this.currentQuestion].answers[i] + '">' + questions[this.currentQuestion].answers[i]+ '</button>');
     }
   },
   nextQuestion: function(){
     //I'm a little worried they might be confused as to why they cant use "this" here
-    console.log('question reset');
-    game.counter = 3; ////////////////////////CHANGE ME
+    game.counter = countStartNumber;
+    $('#counter-number').html(game.counter);
     game.currentQuestion++;
     game.loadQuestion();
   },
   timeUp: function (){
     clearInterval(timer);
+    $('#counter-number').html(game.counter);
+
     panel.html('<h2>Out of Time!</h2>');
     panel.append('<h3>The Correct Answer was:' + questions[this.currentQuestion].correctAnswer);
     panel.append('<img src="' + questions[this.currentQuestion].image + '" />');
@@ -132,17 +127,28 @@ var game = {
     }
   },
   results: function() {
+    clearInterval(timer);
+
     panel.html('<h2>All done, heres how you did!</h2>');
+    $('#counter-number').html(game.counter);
     panel.append('<h3>Correct Answers: ' + game.correct + '</h3>');
     panel.append('<h3>incorrect Answers: ' + game.incorrect + '</h3>');
     panel.append('<h2 id="start-over">Start Over?</h2>');
   },
+  clicked: function(e) {
+    clearInterval(timer);
+
+    if ($(e.target).data("name") === questions[this.currentQuestion].correctAnswer){
+      this.answeredCorrectly();
+    } else {
+      this.answeredIncorrectly();
+    }
+  },
   answeredIncorrectly: function() {
-    console.log(this);
     game.incorrect++;
     clearInterval(timer);
     panel.html('<h2>Nope!</h2>');
-    panel.append('<h3>The Correct Answer was:' + questions[game.currentQuestion].correctAnswer);
+    panel.append('<h3>The Correct Answer was:' + questions[game.currentQuestion].correctAnswer + '</h3>');
     panel.append('<img src="' + questions[game.currentQuestion].image + '" />');
 
     if (game.currentQuestion === questions.length - 1){
@@ -152,9 +158,8 @@ var game = {
     }
   },
   answeredCorrectly: function(){
-    console.log(this);
-    game.correct++;
     clearInterval(timer);
+    game.correct++;
     panel.html('<h2>Correct!</h2>');
     panel.append('<img src="' + questions[game.currentQuestion].image + '" />');
 
