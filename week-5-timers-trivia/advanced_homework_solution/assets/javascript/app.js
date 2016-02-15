2d126efd90212a418e0d249e04234786175ee9a6
Owens O'Brien <owensobrien@gmail.com>
2016-02-15 16:49:24 -0500

    adjust hw folder structure and removed comments

--- week-5-timers-trivia/advanced_homework_solution/assets/javascript/app.js
+++ week-5-timers-trivia/advanced_homework_solution/assets/javascript/app.js
@@ -34,42 +34,42 @@ var questions = [{
   question: "What was the first full length CGI movie?",
   answers: ["A Bug's Life", "Monsters Inc.", "Toy Story", "The Lion King"],
   correctAnswer: "Toy Story",
-  image:"img/toystory.gif"
+  image:"assets/images/toystory.gif"
 }, {
   question: "Which of these is NOT a name of one of the Spice Girls?",
   answers: ["Sporty Spice", "Fred Spice", "Scary Spice", "Posh Spice"],
   correctAnswer: "Fred Spice",
-  image:"img/spicegirls.gif"
+  image:"assets/images/spicegirls.gif"
 }, {
   question: "Which NBA team won the most titles in the 90s?",
   answers: ["New York Knicks", "Portland Trailblazers", "Los Angeles Lakers", "Chicago Bulls"],
   correctAnswer: "Chicago Bulls",
-  image:"img/bulls.gif"
+  image:"assets/images/bulls.gif"
 }, {
   question: 'Which group released the hit song, "Smells Like Teen Spirit"?',
   answers: ["Nirvana", "Backstreet Boys", "The Offspring", "No Doubt"],
   correctAnswer: "Nirvana",
-  image:"img/nirvanabark.gif"
+  image:"assets/images/nirvanabark.gif"
 }, {
   question: 'Which popular Disney movie featured the song, "Circle of Life"?',
   answers: ["Aladdin", "Hercules", "Mulan", "The Lion King"],
   correctAnswer: "The Lion King",
-  image:"img/lionking.gif"
+  image:"assets/images/lionking.gif"
 }, {
   question: 'Finish this line from the Fresh Prince of Bel-Air theme song: "I whistled for a cab and when it came near, the license plate said..."',
   answers: ["Dice", "Mirror", "Fresh", "Cab"],
   correctAnswer: "Fresh",
-  image:"img/fresh.gif"
+  image:"assets/images/fresh.gif"
 }, {
   question: "What was Doug's best friend's name?",
   answers: ["Skeeter", "Mark", "Zach", "Cody"],
   correctAnswer: "Skeeter",
-  image:"img/skeeter.gif"
+  image:"assets/images/skeeter.gif"
 }, {
   question: "What was the name of the principal at Bayside High in Saved By The Bell?",
   answers: ["Mr.Zhou", "Mr.Driggers", "Mr.Belding", "Mr.Page"],
   correctAnswer: "Mr.Belding",
-  image:"img/belding.gif"
+  image:"assets/images/belding.gif"
 }];
 
 
@@ -98,7 +98,6 @@ var game = {
     }
   },
   nextQuestion: function(){
-    //I'm a little worried they might be confused as to why they cant use "this" here
     game.counter = countStartNumber;
     $('#counter-number').html(game.counter);
     game.currentQuestion++;
@@ -113,9 +112,9 @@ var game = {
     panel.append('<img src="' + questions[this.currentQuestion].image + '" />');
 
     if (game.currentQuestion === questions.length - 1){
-      setTimeout(game.results, 3 * 1000);////////////////////////CHANGE ME
+      setTimeout(game.results, 3 * 1000);
     } else {
-      setTimeout(game.nextQuestion, 3 * 1000);////////////////////////CHANGE ME
+      setTimeout(game.nextQuestion, 3 * 1000);
     }
   },
   results: function() {
@@ -145,9 +144,9 @@ var game = {
     panel.append('<img src="' + questions[game.currentQuestion].image + '" />');
 
     if (game.currentQuestion === questions.length - 1){
-      setTimeout(game.results, 3 * 1000);////////////////////////CHANGE ME
+      setTimeout(game.results, 3 * 1000);
     } else {
-      setTimeout(game.nextQuestion, 3 * 1000);////////////////////////CHANGE ME
+      setTimeout(game.nextQuestion, 3 * 1000);
     }
   },
   answeredCorrectly: function(){
@@ -157,9 +156,9 @@ var game = {
     panel.append('<img src="' + questions[game.currentQuestion].image + '" />');
 
     if (game.currentQuestion === questions.length - 1){
-      setTimeout(game.results, 3 * 1000);////////////////////////CHANGE ME
+      setTimeout(game.results, 3 * 1000);
     } else {
-      setTimeout(game.nextQuestion, 3 * 1000);////////////////////////CHANGE ME
+      setTimeout(game.nextQuestion, 3 * 1000);
     }
   },
   reset: function(){
