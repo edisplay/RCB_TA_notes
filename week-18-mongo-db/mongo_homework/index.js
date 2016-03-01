c4e846e041a46df579da4dfaa659480fe5c977ed
zhenpanda <zhentzhou@gmail.com>
2016-02-29 21:57:15 -0500

    saving

--- week-18-mongo-db/mongo_homework/index.js
+++ week-18-mongo-db/mongo_homework/index.js
@@ -28,118 +28,151 @@ app.use(bodyParser.urlencoded({
     extended: false
 }));
 
+//custom date 
+var makeDate = function() {
+
+    var d = new Date();
+    var formatedDate = "";
+    formatedDate = formatedDate + (d.getMonth() + 1) + "_";
+    formatedDate = formatedDate + d.getDate() + "_";
+    formatedDate = formatedDate + d.getFullYear();
+    //console.log(formatedDate);
+
+	return formatedDate;
+}
+
 //scrape ny times
 var times = function(input, cb) {
 
-	if (input == 'fetch') {
-		var url = "http://www.nytimes.com";
-		request(url, function(err, res, body) {
+    if (input == 'fetch') {
+        var url = "http://www.nytimes.com";
+        request(url, function(err, res, body) {
 
-			var $ = cheerio.load(body);
-			var obj = {};
+            var $ = cheerio.load(body);
+            var obj = {};
 
-			//str.replace(/\s+/g, ' ').trim()
-		    $('.story-heading').each(function(i, element){
-		    	var msg = $(this).text();
-		     	var msgNeat = msg.replace(/(\r\n|\n|\r|\t|\s+)/gm," ").trim();
-		     	obj[i] = [msgNeat]
-		    });
+            //str.replace(/\s+/g, ' ').trim()
+            $('.story-heading').each(function(i, element) {
+                var msg = $(this).text();
+                var msgNeat = msg.replace(/(\r\n|\n|\r|\t|\s+)/gm, " ").trim();
+                obj[i] = [msgNeat]
+            });
 
-		    //appending the summary
-		    $('.summary').each(function(i, element){
-		    	var sumy = $(this).text();
-		     	var sumyNeat = sumy.replace(/(\r\n|\n|\r|\t|\s+)/gm," ").trim();
-		     	obj[i].push(sumyNeat);
-		    });  
-		    //console.log(obj);
+            //appending the summary
+            $('.summary').each(function(i, element) {
+                var sumy = $(this).text();
+                var sumyNeat = sumy.replace(/(\r\n|\n|\r|\t|\s+)/gm, " ").trim();
+                obj[i].push(sumyNeat);
+            });
+            //console.log(obj);
 
-			var d = new Date();
-		    var formatedDate = "";
-		    formatedDate = formatedDate + (d.getMonth() + 1) + "_";
-		    formatedDate = formatedDate + d.getDate() + "_";
-		    formatedDate = formatedDate + d.getFullYear();
-		    //console.log(formatedDate);
-			
+            console.log("start fetching");
+            var myCount = function(cb) {
+                //check if date already exit in the database
+                mycollection.find().count(function(err, doc_count) {
+                    //count the collection
+                    console.log(doc_count, "total items");
+                    //use the callback to return the count
+                    cb(doc_count);
+                });
+            }
 
-			console.log("start fetching");
-			var myCount = function(cb) {
-			    //check if date already exit in the database
-				mycollection.find().count(function(err, doc_count){
-					//count the collection
-					console.log(doc_count, "total items");
-					//use the callback to return the count
-					cb( doc_count );
-				});
-			}
+            var formatedDate = makeDate();
 
-			//use callback to grab the count
-			myCount(function(c){
-				//callback c returns the doc_count
-				console.log(c, "this is the db count");
-				// if collection empty
-				if (c == 0) {
-					console.log("Didn't find any object in collection, do the scrape.");
-					mycollection.save({
-		 		    	"nyt": obj,
-		 		    	date: formatedDate,
-		 		    	myId: 1
-					});
-				}else{
-					mycollection.findOne({
-					    myId: c
-					}, function(err, doc) {
-					    console.log(doc.date);
-					    if (doc.date != formatedDate) {
-					    	console.log("this is a new item need to be saved");
-					    	var newId = c + 1;
-							mycollection.save({
-				 		    	"nyt": obj,
-				 		    	date: formatedDate,
-				 		    	myId: newId
-							});
-					    }else{
-					    	console.log("we already got this, go on");
-					    }
-					});
-				}								
+            //use callback to grab the count
+            myCount(function(c) {
+                //callback c returns the doc_count
+                console.log(c, "this is the db count");
+                // if collection empty
+                if (c == 0) {
+                    console.log("Didn't find any object in collection, do the scrape.");
+                    mycollection.save({
+                        "nyt": obj,
+                        date: formatedDate,
+                        myId: 1
+                    });
+                } else {
+                    //if database doesn't have it save it as new
+                    mycollection.findOne({
+                        myId: c
+                    }, function(err, doc) {
+                        console.log(doc.date);
+                        if (doc.date != formatedDate) {
+                            console.log("this is a new item need to be saved");
+                            var newId = c + 1;
+                            mycollection.save({
+                                "nyt": obj,
+                                date: formatedDate,
+                                myId: newId
+                            });
+                        } else {
+                            console.log("we already got this, go on");
+                        }
+                    });
+                }
 
-			})
+            })
+        });
+    } else if (input == 'check') {
+        //descending sort
+        mycollection.find().sort({ myId: -1 }, function(err, doc) {
+            console.log(doc[0].date, doc[0].myId);
+            cb(doc);
+        });
 
+    } else if (input == 'update') {
+        //descending sort
+        mycollection.find().sort({ myId: -1 }, function(err, doc) {
+            console.log(doc[0].date, doc[0].myId);
+            //cb(doc);
+        });
+    }
+}
 
-		});
-	}else if (input == 'check') {
-		//descending sort
-		mycollection.find().sort({ myId: -1 }, function(err, doc){
-			console.log(doc[0].date, doc[0].myId);
-			cb(doc);
-		});
+//save notes into mongodb
+var notes = function(input, data, cb) {
 
-	}else if (input == 'update') {
-		//descending sort
-		mycollection.find().sort({ myId: -1 }, function(err, doc){
-			console.log(doc[0].date, doc[0].myId);
-			//cb(doc);
-		});
-	}
+    if (input == "save") {
+    	console.log(data);
+
+        mynotes.save({
+			articleId: data.id,
+			articleDate: data.date,
+			articleNote: data.note
+        });
+
+    	cb(data);
+    };
+    //finish the save
+
 }
 
 //basic route use cb return json data from mongodb
 app.get('/home', function(req, res) {
-	res.render('home');
+    res.render('home');
 });
 
 app.get('/check', function(req, res) {
-	times('check', function(data) {
-		res.json(data)
-	})
+    times('check', function(data) {
+        res.json(data)
+    })
 });
 
-//get save data
+//get grab web scrape
 app.post('/fetch', function(req, res) {
-	times('fetch');
-	res.send('success');
+    times('fetch');
+    res.send('success');
 });
 
-app.listen(port, function(){
-	console.log("lisenting on port:"+port);
+//get save note
+app.post('/save', function(req, res) {
+    notes('save', req.body, function(data) {
+    	// post req must give back resopnce as json??? wat..ok..
+        res.json(data)
+    })
+});
+
+
+app.listen(port, function() {
+    console.log("lisenting on port:" + port);
 });
--- week-18-mongo-db/mongo_homework/public/assets/javascript/global.js
+++ week-18-mongo-db/mongo_homework/public/assets/javascript/global.js
@@ -4,57 +4,95 @@ $(document).ready(function() {
     //put data in here
     var mongoData;
     var dataCount = 0;
+    var dataDate;
 
     var state = 0;
     var cubeRotateAry = ['show-front', 'show-back', 'show-right', 'show-left', 'show-top', 'show-bottom'];
     var sideAry = ['back', 'right', 'left', 'top', 'bottom', 'front'];
 
     //ajax get data function
-    function populate() {
+    var populate = function() {
         // jQuery AJAX call for JSON
         $.getJSON('/check', function(data) {
             //mongoData = data;
             mongoData = data[0];
-
+            dataDate = mongoData.date; 
             // For each item in our JSON, add a table row and cells to the content string
-            console.log(mongoData);
+            console.log(mongoData, "gotem");
         });
-    };
+    }
+
+    //save notes and clear note taking area
+    var saveNote = function() {
+        $("#note-text").on('click', function() {
+            var t = $("#input-box").val();
+            //console.log(t)
+            var idCount = dataCount - 1;
+
+            $.ajax({
+                type: "POST",
+                dataType: "json",
+                url: '/save',
+                data: {
+                    id: idCount,
+                    date: dataDate,
+                    note: t
+                }
+            }).done(function() {
+                $("#input-box").val("");
+            }).fail(function() {
+                console.log("Sorry. Server unavailable. ");
+            });
+
+        });
+    }
 
     //type animation function
     var typeIt = function() {
+        $("#typewriter").remove();
+        var i = 0;
+        var newsText;
+        
         if (state > 0) {
             side = state - 1;
         } else {
             side = 5;
         }
-        $("#typewriter").remove();
+
         $("." + sideAry[side]).append("<div id='typewriter'></div>");
-
-        var i = 0;
-        var newsText;
-
         //cycle to different story
-        var show = mongoData.nyt[dataCount][0];
         var print = mongoData.nyt[dataCount][1];
         dataCount++;
-
         // type animation for new summary
         (function type() {
-            console.log(newsText);
+            //console.log(newsText);
             newsText = print.slice(0, ++i);
             //return stop when text is equal to the writeTxt
             if (newsText === print) return;
 
             //put in the text via javascript
             $("#typewriter").text(newsText);
-            setTimeout(type, 30);
+            setTimeout(type, 35);
         }());
+    }
+
+    //render headline
+    var headline = function() {
+        var show = "|| Article:" + (dataCount + 1) + " ||";
+        $("#headline").text(show);
+        $("#headline").fadeIn()
+            .css({
+                top: 500,
+                position: 'absolute',
+                left: 855
+            })
+            .animate({
+                top: 255
+            });
     }
 
     //add click event function
     var clickBox = function() {
-
         // $('#cube').removeClass().addClass('show-front');
         $("#cube").on("click", function() {
             //rotate cycle
@@ -65,13 +103,19 @@ $(document).ready(function() {
             }
             $('#cube').removeClass().addClass(cubeRotateAry[state]);
 
+            //animte headline
+            headline();
             //animte text
             typeIt();
+            //start the notes 
+            $("#input-area").show();
+            $("#saved-text").show();
+            saveNote();
         });
     }
 
+    // ajax call to do the scrape
     var fetchData = function() {
-
         $.ajax({
             type: "POST",
             url: '/fetch'
@@ -80,26 +124,30 @@ $(document).ready(function() {
         }).fail(function() {
             alert("Sorry. Server unavailable. ");
         });
-
-    };
+    }
 
     /* running before user action*/
     $('.container').hide();
-    
+
     //getch da data
     fetchData();
 
     //hide stuff before everything
     $("#seek-box").hide();
+    $("#input-area").hide();
+    $("#saved-text").hide();
 
     $("#seek-box").click(function() {
         //put data into html
         populate();
+        //show stuff
         $('.container').show();
         $("#seek-box").hide();
-    });
+
+    })
 
     /* running the functions*/
     clickBox();
 
+    /* note box */
 });
